c4a365b8c22f54bc8b341481038ec7280795e769
Manoj MV <manojmv3019@gmail.com>
2021-02-16 15:21:06 +0530

    Upto section 9

--- services-learning/src/app/new-account/new-account.component.ts
+++ services-learning/src/app/new-account/new-account.component.ts
@@ -1,18 +1,24 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../account.service';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  // providers:[LoggingService]
 })
-export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
-
+export class NewAccountComponent implements OnInit{
+  constructor(private logStatus:LoggingService,
+    private accountService:AccountService
+  ){}
+  ngOnInit(){
+    this.accountService.statusUpdated.subscribe((ststus:string) => {
+      alert('New Status : '+status);
+    })
+  }
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.accountService.addAccount(accountName,accountStatus);
+    // this.logStatus.logStatusChange(accountStatus);
   }
 }
